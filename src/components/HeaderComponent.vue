a254dc934f5b86e035843dbc3892d944664b0f53
ddarahakit2023 <ddarahakit2023@gmail.com>
2024-02-20 11:35:20 +0900

    test2

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -14,7 +14,7 @@
                 </a>
                 <ul class="topMenu">
                     <li v-show="!isLoggedIn">
-                        <a href="/signup/member">회원가입 하자</a>
+                        <a href="/signup/member">회원가입</a>
                     </li>
                     <li v-show="!isLoggedIn">
                         <a href="/login/member">로그인</a>
